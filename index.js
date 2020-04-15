b3c3ecf1431b65953682463c6f3eb1c53bf108d4
soimmy <dayanbiefu7@gmail.com>
2020-04-16 00:42:47 +0900

    add mutation

--- index.js
+++ index.js
@@ -1,14 +1,47 @@
 const { ApolloServer } = require(`apollo-server`)
 
 const typeDefs = `
+
+    # Photo型
+    type Photo {
+        id: ID!
+        url: String!
+        name: String!
+        description: String
+    }
+
+    # return all photo
     type Query {
         totalPhoto: Int!
+        allPhotos: [Photo!]!
+    }
+
+    # ミューテーション Photo型を返す
+    type Mutation {
+        postPhoto(name: String! description: String): Photo!
     }
 `
+var _id = 0
+var photos = []
 
 const resolvers = {
     Query : {
-        totalPhoto: () => 42
+        totalPhoto: () => photos.length,
+        allPhotos: () => photos
+    },
+
+    Mutation : {
+
+        
+        postPhoto(parent, args) {
+
+            var newPhoto = {
+                id: _id++,
+                ...args
+            }
+            photos.push(newPhoto)
+            return newPhoto
+        }
     }
 }
 
