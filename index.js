1fd0db3fab6fb258a830ff810dd057faec2ceab2
soimmy <dayanbiefu7@gmail.com>
2020-04-26 13:35:02 +0900

    separate files

--- index.js
+++ index.js
@@ -1,192 +1,21 @@
-const { ApolloServer } = require(`apollo-server`)
-const { GraphQLScalarType } = require(`graphql`)
-var _id = 0
+const { ApolloServer } = require(`apollo-server-express`)
+const express = require(`express`)
+const expressPlayground = require(`graphql-playground-middleware-express`).default
+const { readFileSync } = require(`fs`)
+const typeDefs = readFileSync(`./typeDefs.graphql`, `UTF-8`)
+const resolvers = require(`./resolvers`)
 
-const typeDefs = `
+var app = express()
 
-    # scalar
-    scalar DateTime
+const server = new ApolloServer({ typeDefs, resolvers })
 
-    # enum
-    enum PhotoCategory {
-        SELFIE
-        PORTRAIT
-        ACTION
-        LANDSCAPE
-        GRAHIC
-    }
+server.applyMiddleware({ app })
 
-    # User
-    type User {
-        githubLogin: ID!
-        name: String
-        avatar: String
-        postedPhotos: [Photo!]!
-        inPhotos: [Photo!]!
-    }
+app.get(`/`, (req, res) => res.end(`Welcome to the PhotoShare API`))
+app.get(`/playground`, expressPlayground({ endpoint: `/graphoql` }))
 
-    # Photo
-    type Photo {
-        id: ID!
-        url: String!
-        name: String!
-        description: String
-        category: PhotoCategory!
-        postedBy: User!
-        taggedUsers: [User!]!
-        created: DateTime!
-    }
+app
+    .listen({ port: 4000}, () =>
+        console.log(`GraphQL Server running @ http://localhost:4000$(server.graphqlPath)`)
+    )
 
-    input PostPhotoInput {
-        name: String!
-        category: PhotoCategory=PORTRAIT
-        description: String
-    }
-
-    # return all photo
-    type Query {
-        totalPhotos: Int!
-        allPhotos(after: DateTime): [Photo!]!
-    }
-
-    # ミューテーション Photo型を返す
-    type Mutation {
-        postPhoto(input: PostPhotoInput!): Photo!
-    }
-`
-var users = [
-    {
-        "githubLogin": "mHattrup",
-        "name": "Mike Hattrup"
-    },
-    {
-        "githubLogin": "gPlake",
-        "name": "Glan Plake"
-    },
-    {
-        "githubLogin": "sSchmidt",
-        "name": "Scot Schmidt"
-    },
-]
-
-var photos = [
-    {
-        "id": "1",
-        "name": "Dropping theHeart Chute",
-        "description": "The heart chute is one of my facorite chutes",
-        "category": "ACTION",
-        "githubUser": "gPlake",
-        "created": "3-28-1997",
-    },
-    {
-        "id": "2",
-        "name": "Enjoying the sunchine",
-        "category": "SELFIE",
-        "githubUser": "sSchmidt",
-        "created": "3-1-1998",
-    },
-    {
-        "id": "3",
-        "name": "Gunbarrel 25",
-        "description": "25 ladps on gunbarrel today",
-        "category": "LANDSCAPE",
-        "githubUser": "sSchmidt",
-        "created": "3-19-2000",
-    },
-]
-var tags = [
-    {
-        "photoID": "1" ,
-        "userID": "gPlake",
-    },
-    {
-        "photoID": "2" ,
-        "userID": "sSchmidt",
-    },
-    {
-        "photoID": "2" ,
-        "userID": "mHattrup",
-    },
-    {
-        "photoID": "2" ,
-        "userID": "gPlake",
-    },
-]
-
-const perseValue = value => new Date(value)
-
-const resolvers = {
-    Query : {
-        totalPhotos: () => photos.length,
-        // allPhotos: (parent, args) => {
-        //     args.after
-        // }
-        allPhotos: () => photos,
-
-    },
-
-    Photo: {
-        url: parent => `http://yoursite.com/img/${parent.id}.jpg`,
-        postedBy: parent => {
-            return users.find(u => u.githubLogin === parent.githubUser)
-        },
-        taggedUsers: parent => tags
-
-            // 対象の写真が関係しているタグの配列を返す
-            .filter(tag => tag.photoID === parent.id)
-
-            // タグの配列ユーザIDの配列に変換する
-            .map(tag => tag.userID)
-
-            // ユーザIDの配列をユーザオブジェクトの配列に変換する
-            .map(userID => users.find(u => u.githubLogin === userID))
-    },
-    User: {
-        postedPhotos: parent => {
-            return photos.filter(p => p.githubUser === parent.githubLogin)
-        },
-        inPhotos: parent => tags
-
-            // 対象のユーザが関係しているタグの配列を返す
-            .filter(tag => tag.userID === parent.id)
-
-            // タグの配列ユーザIDの配列に変換する
-            .map(tag => tag.photoID)
-
-            // 写真IDの配列を写真オブジェクトの配列に変換する
-            .map(userID => users.find(p => p.id === photoID))
-    },
-
-    DateTime : 
-        new GraphQLScalarType({
-            name: `DateTime`,
-            description: `A valid date time value`,
-            parseValue: value => new Date(value),
-            serialize: value => new Date(value).toISOString(),
-            parseLiteral: ast => ast.value,
-    }),
-
-    Mutation : {
-
-        postPhoto(parent, args) {
-            var newPhoto = {
-                id: _id++,
-                ...args.input,
-                created: new Date()
-            }
-            photos.push(newPhoto)
-            return newPhoto
-        }
-    },
-}
-
-const server = new ApolloServer({
-    typeDefs,
-    resolvers,
-})
-
-server
-    .listen()
-    .then(
-        ({url}) => console.log(`GraphQL Service running on ${url}`)
-        )
